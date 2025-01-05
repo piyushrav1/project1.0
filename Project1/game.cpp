5e93581edd3aac553c09f0b02058fd265b7c2853
Piyush Ravi <piyushravi.tiu@gmail.com>
2025-01-05 22:10:33 +0530

    2.02

--- Project1/game.cpp
+++ Project1/game.cpp
@@ -1,7 +1,13 @@
+#define is_down(b) input->buttons[b].is_down
+#define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
+#define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)
+
+float player_pos = 0.f;
+
 internal void
 simulate_game(Input* input) {
 	clear_screen(0xff55010);
+	if (pressed(BUTTON_UP)) player_pos += 1.f;
 
-	if (input->buttons[BUTTON_UP].is_down)
-		draw_rect(0, 0, 0.2, 0.2, 0x00ff22);
+		draw_rect(0, player_pos, 1, 1, 0x00ff22);
 }
--- Project1/renderer.cpp
+++ Project1/renderer.cpp
@@ -13,38 +13,39 @@ clear_screen(u32 color) {
 
 
 
-internal void 
-draw_rect_in_pixels(int x, int y, int width, int height, u32 color) {
+internal void
+draw_rect_in_pixels(int x0, int y0, int x1, int y1, u32 color) {
 
-	x = clamp(0, x, render_state.width);
-	y = clamp(0, y, render_state.height);
-	width = clamp(0, width, render_state.width - x);
-	height = clamp(0, height, render_state.height - y);
+	x0 = clamp(0, x0, render_state.width);
+	x1 = clamp(0, x1, render_state.width);
+	y0 = clamp(0, y0, render_state.height);
+	y1 = clamp(0, y1, render_state.height);
 
-	for (int y0 = y; y0 < y + height; y0++) {
-		for (int x0 = x; x0 < x + width; x0++) {
-			u32* pixel = (u32*)render_state.memory + x0 + y0 * render_state.width;
-			*pixel = color;
+	for (int y = y0; y < y1; y++) {
+		u32* pixel = (u32*)render_state.memory + x0 + y * render_state.width;
+		for (int x = x0; x < x1; x++) {
+			*pixel++ = color;
 		}
 	}
 }
+global_variable float render_scale = 0.01f;
 
 internal void
-draw_rect(float x, float y, float half_size_x, float half_size_y,u32 color) {
+draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
 
-	//change to pixels
-	x *= render_state.height;
-	y *= render_state.height;
-	half_size_x *= render_state.height;
-	half_size_y *= render_state.height;
+	x *= render_state.height * render_scale;
+	y *= render_state.height * render_scale;
+	half_size_x *= render_state.height * render_scale;
+	half_size_y *= render_state.height * render_scale;
 
 	x += render_state.width / 2.f;
 	y += render_state.height / 2.f;
 
+	// Change to pixels
 	int x0 = x - half_size_x;
-	int y0 = y - half_size_y;
 	int x1 = x + half_size_x;
+	int y0 = y - half_size_y;
 	int y1 = y + half_size_y;
 
-	draw_rect_in_pixels(x0, y0, x1 - x0, y1 - y0, color);
+	draw_rect_in_pixels(x0, y0, x1, y1, color);
 }
