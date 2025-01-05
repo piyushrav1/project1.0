d3e9f3194df5258a20d7b1f9e626075f02327346
Piyush Ravi <piyushravi.tiu@gmail.com>
2025-01-05 23:55:07 +0530

    2.04

--- Project1/game.cpp
+++ Project1/game.cpp
@@ -2,19 +2,45 @@
 #define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)
 
-float player_pos_x = 0.f;
-float player_pos_y = 0.f;
+float player_1_p, player_1_dp, player_2_p, player_2_dp;
+//float player_pos_y = 0.f;
 
 internal void
 simulate_game(Input* input, float dt) {
 	clear_screen(0xff55010);
+	
+	draw_rect(0, 0, 85, 45, 0xffaa33);
 
-	float speed = 50.f; //units per second
-	if (is_down(BUTTON_UP)) player_pos_y += speed * dt;
-	if (is_down(BUTTON_DOWN)) player_pos_y -= speed * dt;
-	if (is_down(BUTTON_LEFT)) player_pos_x -= speed * dt;
-	if (is_down(BUTTON_RIGHT)) player_pos_x += speed * dt;
+	//float speed = 5.f; //units per second
+	//if (pressed(BUTTON_UP)) player_pos_y += speed;
+	//if (pressed(BUTTON_DOWN)) player_pos_y -= speed;
+	//if (pressed(BUTTON_LEFT)) player_pos_x -= speed;
+	//if (pressed(BUTTON_RIGHT)) player_pos_x += speed;
+		//draw_rect(player_pos_x, player_pos_y, 1, 1, 0x00ff22);
+	//float speed = 50.f; //units per second
+
+	float player_1_ddp = 0.f;
+	if (is_down(BUTTON_UP)) player_1_ddp += 2000;
+	if (is_down(BUTTON_DOWN)) player_1_ddp -= 2000;
+
+	float player_2_ddp = 0.f;
+	if (is_down(BUTTON_W)) player_2_ddp += 2000;
+	if (is_down(BUTTON_S)) player_2_ddp -= 2000;
 
 
-		draw_rect(player_pos_x, player_pos_y, 1, 1, 0x00ff22);
+	player_1_ddp -= player_1_dp * 10.f;
+
+	player_1_p = player_1_p + player_1_dp * dt + player_1_ddp * dt * dt * .5f;
+	player_1_dp = player_1_dp + player_1_ddp * dt;
+
+	player_2_ddp -= player_2_dp * 10.f;
+
+	player_2_p = player_2_p + player_2_dp * dt + player_2_ddp * dt * dt * .5f;
+	player_2_dp = player_2_dp + player_2_ddp * dt;
+
+
+
+		draw_rect(0, 0, 1, 1, 0xffffff);
+		draw_rect(80, player_1_p, 2.5, 12, 0xff0000);
+		draw_rect(-80, player_2_p, 2.5, 12, 0xff0000);
 }
