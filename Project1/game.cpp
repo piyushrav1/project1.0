4e5b5eaa85e3d213ce2f8d8aadaaca4683d375ff
Piyush Ravi <piyushravi.tiu@gmail.com>
2025-01-06 01:29:54 +0530

    2.05

--- Project1/game.cpp
+++ Project1/game.cpp
@@ -3,13 +3,15 @@
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)
 
 float player_1_p, player_1_dp, player_2_p, player_2_dp;
+float arena_half_size_x = 85, arena_half_size_y = 45;
+float player_half_size_x = 2.5, player_half_size_y = 12;
 //float player_pos_y = 0.f;
 
 internal void
 simulate_game(Input* input, float dt) {
 	clear_screen(0xff55010);
 	
-	draw_rect(0, 0, 85, 45, 0xffaa33);
+	draw_rect(0, 0, arena_half_size_x, arena_half_size_y, 0xffaa33);
 
 	//float speed = 5.f; //units per second
 	//if (pressed(BUTTON_UP)) player_pos_y += speed;
@@ -33,14 +35,32 @@ simulate_game(Input* input, float dt) {
 	player_1_p = player_1_p + player_1_dp * dt + player_1_ddp * dt * dt * .5f;
 	player_1_dp = player_1_dp + player_1_ddp * dt;
 
+	if (player_1_p + player_half_size_y > arena_half_size_y) {
+		player_1_p = arena_half_size_y - player_half_size_y;
+		player_1_dp *= -1;
+	}
+	else if (player_1_p - player_half_size_y < -arena_half_size_y) {
+		player_1_p = -arena_half_size_y + player_half_size_y;
+		player_1_dp *= -1;
+	}
+
 	player_2_ddp -= player_2_dp * 10.f;
 
 	player_2_p = player_2_p + player_2_dp * dt + player_2_ddp * dt * dt * .5f;
 	player_2_dp = player_2_dp + player_2_ddp * dt;
 
+	if (player_2_p + player_half_size_y > arena_half_size_y) {
+		player_2_p = arena_half_size_y - player_half_size_y;
+		player_2_dp *= -1;
+	}
+	else if (player_2_p - player_half_size_y < -arena_half_size_y) {
+		player_2_p = -arena_half_size_y + player_half_size_y;
+		player_2_dp *= -1;
+	}
+
 
 
 		draw_rect(0, 0, 1, 1, 0xffffff);
-		draw_rect(80, player_1_p, 2.5, 12, 0xff0000);
-		draw_rect(-80, player_2_p, 2.5, 12, 0xff0000);
+		draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000);
+		draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xff0000);
 }
